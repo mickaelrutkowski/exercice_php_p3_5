4990e13490a08bb4b784b7e8d616b31a1d1095ed
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-21 09:28:49 +0100

    exercice_php_P3_5

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  $num = 1;
+  while ($num <15){
+    echo ' ' . 'On y arrive presque.';
+   $num++;
+  }
+ ?>
+</body>
+</html>
